e1ea10b0d34a15db3803e31ae50c8f66919c4b04
Mr. Matt <system9@composition9.com>
2012-10-31 15:54:43 -0600

    simplify apps

--- rails/recipes/app_dependencies.rb
+++ rails/recipes/app_dependencies.rb
@@ -11,47 +11,39 @@ end
 if node[:active_applications]
   
   node[:active_applications].each do |name, conf|
-
-    app = search(:apps, "id:#{conf[:app_name] || name}").first
-    app_name = name
-    app_root = "/u/apps/#{app_name}"
-
     %w(config tmp sockets log tmp/pids system bin).each do |dir|
-      directory "/u/apps/#{app_name}/shared/#{dir}" do
+      directory "/u/apps/#{name}/shared/#{dir}" do
         recursive true
         owner "app"
         group "app"
       end
     end
             
-    if app
+    if conf[:packages]
+      conf[:packages].each do |package_name|
+        package package_name
+      end      
+    end
 
-      if app[:packages]
-        app[:packages].each do |package_name|
-          package package_name
-        end      
-      end
-
-      if app[:gems]
-        app[:gems].each do |g|
-          if g.is_a? Array
-            gem_package g.first do
-              version g.last
-            end
-          else
-            gem_package g
+    if conf[:gems]
+      conf[:gems].each do |g|
+        if g.is_a? Array
+          gem_package g.first do
+            version g.last
           end
+        else
+          gem_package g
         end
       end
-    
-      if app[:symlinks]
-        app[:symlinks].each do |target, source|
-          link target do
-            to source
-          end
+    end
+  
+    if conf[:symlinks]
+      conf[:symlinks].each do |target, source|
+        link target do
+          to source
         end
       end
-    end              
+    end
   end
 else
   Chef::Log.info "Add an :active_applications attribute to configure this node's Rails apps"
