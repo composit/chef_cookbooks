d292901feeebb53485766910b25357c221df8779
Mr. Matt <system9@composition9.com>
2012-11-19 13:40:15 -0700

    remove rvm

--- rvm/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer        "Matthew Rose"
-maintainer_email  "system9@composition9.com"
-description       "Installs and configures RVM"
-version           "0.0.1"
-depends           "curl"
-depends           "git-core"
