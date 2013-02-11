4190774c730d68f1d3c3633f87101973979905eb
Mr. Matt <matt@composition9.com>
2013-02-11 06:10:03 -0700

    metadata

--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -1,5 +1,5 @@
-maintainer        "Joshua Sierles"
-maintainer_email  "joshua@diuvia.net"
+maintainer        "Matthew Rose"
+maintainer_email  "system9@composition9.com"
 description       "Configures unicorn"
 version           "0.2"
-depends           "ruby"
+#depends           "ruby"
